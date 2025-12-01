9422ff05e15bf8bb16517ecc54f316d5bf50fe54
mwjt <mwjt33@gmail.com>
2025-12-01 15:42:22 +0100

    01.2

--- 01.py
+++ 01.py
@@ -1,6 +1,7 @@
 instructions = []
 pos = 50
-total = 0
+total1 = 0
+total2 = 0
 
 with open("01.txt", 'r') as file:
     for line_number, line in enumerate(file, 1):
@@ -9,16 +10,27 @@ with open("01.txt", 'r') as file:
             instructions.append(line)
 
 for instruction in instructions:
-    clicks = int(instruction[1:]) % 100
+    oldPos = pos
+    oldTotal = total2
+    clicks = int(instruction[1:])
     if instruction[:1] == 'L':
-        pos -= clicks
-        if pos < 0:
-            pos += 100
-    else:
-        pos += clicks
-        if pos > 99:
-            pos -= 100
+        while clicks > 0:
+            pos -= 1
+            if pos < 0:
+                pos += 100
+            if pos == 0:
+                total2 += 1
+            clicks -= 1
+    else: 
+        while clicks > 0:
+            pos += 1
+            if pos > 99:
+                pos -= 100
+            if pos == 0:
+                total2 += 1
+            clicks -= 1
     if pos == 0:
-        total += 1
+        total1 += 1
 
-print(total)
+print(total1)
+print(total2)
